bded25104b923af0d7d1944fc64df781639a6463
d03x <dadanhidyt@gmail.com>
2026-01-05 14:28:34 +0700

    update

---
+++ app/Http/Controllers/Guru/QuizController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guru;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
